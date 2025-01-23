f8d9af80d6ff955efb11f2cc4b289bdb98aaf196
Maximilian Lang <lang.maximilian1997@gmail.com>
2025-01-23 20:24:41 +0100

    add verbose flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -18,9 +19,18 @@ func newCmdRoot() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:   "dotx",
 		Short: "The next generation dotfile manager",
+
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if viper.GetBool("verbose") {
+				slog.SetLogLoggerLevel(slog.LevelDebug)
+			}
+		},
 	}
 
-	rootCmd.PersistentFlags().StringP("dir", "d", "$HOME/.dotfiles", "dotfiles directory")
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "more verbose output")
+	rootCmd.PersistentFlags().StringP("dir", "d", "$HOME/.dotfiles", "dotfiles directory on your machine")
+
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
 	viper.BindPFlag("dir", rootCmd.PersistentFlags().Lookup("dir"))
 
 	rootCmd.AddCommand(newInitCmd())
