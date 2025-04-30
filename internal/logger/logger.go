8bdcca3da285a01343acf18ae42039912974e885
Maximilian Lang <lang.maximilian1997@gmail.com>
2025-04-30 10:04:51 +0200

    Set custom log styles for different log levels
    Added styling for debug, info, warn, and error levels using `lipgloss` to enhance log clarity and readability. Each log level now has a distinct appearance with specific colors, padding, and formatting.

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,10 +1,14 @@
 package logger
 
 import (
+	"github.com/charmbracelet/lipgloss"
 	"os"
+	"strings"
 
 	charmLogger "github.com/charmbracelet/log"
 )
+
+var log *charmLogger.Logger
 
 type Level int
 
@@ -15,7 +19,20 @@ const (
 	ErrorLevel
 )
 
-var log *charmLogger.Logger
+func (level Level) string() string {
+	switch level {
+	case DebugLevel:
+		return "debug"
+	case InfoLevel:
+		return "info"
+	case WarnLevel:
+		return "warn"
+	case ErrorLevel:
+		return "error"
+	}
+
+	return ""
+}
 
 func Debug(msg any, keys ...any) {
 	log.Debug(msg, keys...)
@@ -48,8 +65,33 @@ func SetLevel(level Level) {
 }
 
 func init() {
+	styles := charmLogger.DefaultStyles()
+	styles.Levels[charmLogger.DebugLevel] = lipgloss.NewStyle().
+		SetString(strings.ToUpper(DebugLevel.string())).
+		Bold(true).
+		Padding(0, 1, 0, 1).
+		Width(7).
+		Foreground(lipgloss.Color("#5f5fff"))
+	styles.Levels[charmLogger.InfoLevel] = lipgloss.NewStyle().
+		SetString(strings.ToUpper(InfoLevel.string())).
+		Bold(true).
+		Padding(0, 1, 0, 1).
+		Width(7).
+		Foreground(lipgloss.Color("#008000"))
+	styles.Levels[charmLogger.WarnLevel] = lipgloss.NewStyle().
+		SetString(strings.ToUpper(WarnLevel.string())).
+		Bold(true).
+		Padding(0, 1, 0, 1).
+		Width(7).
+		Foreground(lipgloss.Color("#ff8800"))
+	styles.Levels[charmLogger.ErrorLevel] = lipgloss.NewStyle().
+		SetString(strings.ToUpper(ErrorLevel.string())).
+		Bold(true).
+		Padding(0, 1, 0, 1).
+		Width(7).
+		Foreground(lipgloss.Color("#ff0000"))
+
 	log = charmLogger.New(os.Stderr)
-	log.SetReportTimestamp(false)
-	log.SetReportCaller(false)
+	log.SetStyles(styles)
 	log.SetLevel(charmLogger.InfoLevel)
 }
